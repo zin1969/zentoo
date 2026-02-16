cbb8b6f7e45719dcf54e0c2bb0453860d5fb5482
Hitoshi Iwamasa <hitoshi.iwamasa.vvat@willway.jp>
2026-02-16 18:11:04 +0900

    feat: add TokenAuthenticator for token-based authentication

---
+++ app/session_handlers/token_authenticator.rb
@@ -0,0 +1,15 @@
+module SessionHandlers
+  class Authenticator
+    InvalidToken = Class.new(StandardError)
+
+    def authenticate!(token)
+      raise InvalidToken unless token == 'dummy-token'
+
+      # 仮のユーザー情報
+      OpenStruct.new(
+        id: 1,
+        name: 'masa'
+      )
+    end
+  end
+end
